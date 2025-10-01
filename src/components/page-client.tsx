b4d0ee9cffbd30c1548d4f2c1ff677942c504036
Nick Weindel <nickweindel@proton.me>
2025-09-30 23:12:09 -0500

    do some more setup and get the selector component set up

--- src/components/page-client.tsx
+++ src/components/page-client.tsx
@@ -1,13 +1,28 @@
 "use client";
 
+import { useState } from "react";
+
 import { PageHeader } from "@/components/shared/page-header";
+import { Selector } from "@/components/shared/selector";
 
 export default function PageClient({ user }: { user: any }) {
+  const [internationalOrDomestic, setInternationalOrDomestic] = useState("Domestic");
+  const [countryOrContinent, setCountryOrContinent] = useState("Country");
+
   return (
     <div>
       <PageHeader user={user} />
-      <div className="flex flex-row gap-3 p-3">
-        Hello World
+      <div className="flex flex-row gap-3 p-3 justify-end">
+        {internationalOrDomestic === "International" && (
+          <Selector 
+            category="countryOrContinent"
+            value={countryOrContinent}
+            onValueChange={setCountryOrContinent} />
+        )}
+        <Selector 
+          category="internationalOrDomestic"
+          value={internationalOrDomestic}
+          onValueChange={setInternationalOrDomestic} />
       </div>
     </div>
   );
